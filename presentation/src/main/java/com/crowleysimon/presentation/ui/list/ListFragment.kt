45ec0e33f13891bfc0dbe7ce84a799f1cc15dd8f
Simon Crowley <crowleysimondev@gmail.com>
2020-02-08 10:47:19 +1000

    Applied code formatter to all existing files

--- presentation/src/main/java/com/crowleysimon/presentation/ui/list/ListFragment.kt
+++ presentation/src/main/java/com/crowleysimon/presentation/ui/list/ListFragment.kt
@@ -7,30 +7,30 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.crowleysimon.presentation.R
 import com.crowleysimon.presentation.ui.list.view.epoxyView
-import kotlinx.android.synthetic.main.fragment_list.*
+import kotlinx.android.synthetic.main.fragment_list.listView
 
 class ListFragment : Fragment() {
 
-    private val someList = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+  private val someList = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_list, container, false)
-    }
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    return inflater.inflate(R.layout.fragment_list, container, false)
+  }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
 
-        listView.withModels {
-            someList.forEach {
-                epoxyView {
-                    id(it)
-                    title(it.toString())
-                }
-            }
+    listView.withModels {
+      someList.forEach {
+        epoxyView {
+          id(it)
+          title(it.toString())
         }
+      }
     }
+  }
 }
