266b2b4a9417ed38a48470a35c0594fe619a358e
Simon Crowley <crowleysimondev@gmail.com>
2020-02-09 09:17:07 +1000

    Added Koin DI for db and api service
    Not entirely sure just yet if the db injection was necessary

--- presentation/src/main/java/com/crowleysimon/presentation/PokedexApp.kt
+++ presentation/src/main/java/com/crowleysimon/presentation/PokedexApp.kt
@@ -1,6 +1,9 @@
 package com.crowleysimon.presentation
 
 import android.app.Application
+import com.crowleysimon.presentation.injection.applicationModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 import timber.log.Timber
 
 class PokedexApp : Application() {
@@ -8,6 +11,10 @@ class PokedexApp : Application() {
   override fun onCreate() {
     super.onCreate()
     configureTimber()
+    startKoin {
+      androidContext(this@PokedexApp)
+      modules(applicationModule)
+    }
   }
 
   private fun configureTimber() {
